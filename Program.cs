c1156b0790ec6cfc366855bcee06381e51f70c05
Admin <Admin@DESKTOP-POQE336>
2019-05-22 12:08:17 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("enter first number");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("enter second number");
+            b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a - b);
+            Console.ReadLine();
+        }
+    }
+}
